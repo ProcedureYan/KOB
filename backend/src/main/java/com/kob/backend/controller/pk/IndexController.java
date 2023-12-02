3de9641171d2508ac687d9950cc987aa1b264b76
Sun Jieyan <procedureyan@outlook.com>
2023-12-02 19:02:53 +0800

    项目骨架(小更改)

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -1,10 +1,10 @@
-package com.kob.backend.controler.pk;
+package com.kob.backend.controller.pk;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/pk/")
-public class IndexControler {
+public class IndexController {
 
 }
