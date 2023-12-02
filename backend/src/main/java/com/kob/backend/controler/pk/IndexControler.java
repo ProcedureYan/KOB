e08f2a74d8df8d7a28ef071a6b69f46c81246aee
Sun Jieyan <procedureyan@outlook.com>
2023-12-02 18:14:38 +0800

    backend init

---
+++ backend/src/main/java/com/kob/backend/controler/pk/IndexControler.java
@@ -0,0 +1,10 @@
+package com.kob.backend.controler.pk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pk/")
+public class IndexControler {
+
+}
