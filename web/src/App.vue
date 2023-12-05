e2994d00a3c9a5e0c367de57d10b2409f6f1f836
Sun Jieyan <procedureyan@outlook.com>
2023-12-05 20:13:24 +0800

    登录注册模块初步完成

--- web/src/App.vue
+++ web/src/App.vue
@@ -11,6 +11,25 @@ import "bootstrap/dist/js/bootstrap"
 export default {
   components: {
     NavBar
+  },
+  setup() {
+
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "yxc2",
+    //     password: "123",
+    //     confirmedPassword: "123",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
   }
 }
 </script>
